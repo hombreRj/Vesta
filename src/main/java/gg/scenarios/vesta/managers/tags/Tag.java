58ff27a758e478669e895b45404bf5f0514af73d
rj <rawlinsicloud@gmail.com>
2020-03-03 20:22:32 -0800

    started work on tags

--- src/main/java/gg/scenarios/vesta/managers/tags/Tag.java
+++ src/main/java/gg/scenarios/vesta/managers/tags/Tag.java
@@ -1,4 +1,30 @@
 package gg.scenarios.vesta.managers.tags;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+@Getter
+@Setter
 public class Tag {
+
+    public static HashMap<UUID, Tag> tags = new HashMap<>();
+
+    private UUID uuid;
+    private String name;
+    private String prefix;
+    private String permission;
+
+    public Tag(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public Tag(String prefix, String name) {
+        this.prefix = prefix;
+        this.name = name;
+        this.uuid = UUID.randomUUID();
+        tags.put(uuid, this);
+    }
 }
